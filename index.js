5be94a86beb851b0d481df97f5416b24ee12e5ba
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-08-25 16:11:58 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seen = {};
+  let result = false;
+  for (let i = 0; i < array.length; i++) {
+    const currentValue = array[i];
+    const neededValue = target - currentValue;
+    if (seen[neededValue]) {
+      result = true;
+    } else {
+      seen[currentValue] = currentValue;
+    }
+  }
+  return result;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
   Add your pseudocode here
-*/
+  seen {}
+  result = false
+  for each element in array {
+    currentValue = elelemet
+    neededvValue = target - currentValue
+    if seen has the neededvValue return true
+    else add currentValue to seen
+  }
+  return result
+  */
 
 /*
   Add written explanation of your solution here
+  cheack weather there is a pair of elements in the array that could add up to the target value
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +52,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 4, 2, 0], 3));
 }
 
 module.exports = hasTargetSum;
